5ec31b558ee0aeb106dc6559f0b20515bf8f6d6d
Marouane_aittamanaite <85191119+aittamanaite@users.noreply.github.com>
2023-06-16 18:47:13 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 /**
- * main - it puts out output using prinft fonction
+ * main -Entry point
  *
- * Description: a c programme that print a String
+ * main - it puts out output using printf
  *
- * Return: always 0 (success)
+ * Description: a c programme
  *
-*/ 
+ * Return: Always 0
+ *
+*/
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
 }
