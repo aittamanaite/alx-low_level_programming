7d6f0a0006e198e37937c0ff8bc158bffe4f5ae2
aittamanaite <aittamanaite2001@gmail.com>
2023-06-15 19:10:02 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description : A C programme that uses puts fuctions()
+ *
+ * Return :allways 0 (sucess)
+ *
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
