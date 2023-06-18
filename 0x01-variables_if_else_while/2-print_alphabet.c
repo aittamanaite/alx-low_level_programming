6cb62287376efc192d8e41f72c75ba528b39cfef
aittamanaite <aittamanaite2001@gmail.com>
2023-06-18 07:14:49 -0700

    print_alphabit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main -Entry point
+ *
+ * Description: a c programme
+ *
+ * Return: 0;
+*/
+int main(void)
+{
+        char c= 'a';
+        while (c <= 'z')
+        {
+                putchar(c);
+                c++;
+        }
+        putchar('\n');
+
+
+}
