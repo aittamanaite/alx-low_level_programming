365369f7ae10592e8f3693e6a855bed54354397c
aittamanaite <aittamanaite2001@gmail.com>
2023-06-17 17:11:54 -0700

    positive_negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: ac programme
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	n = rand() - RAND_MAX / 2;
+	if ( n > 0 )
+	       printf("%d is positive", n)
+	else if( n == 0)
+ 		printf("%d is 0", n)
+	else
+		print("%d is negative", n)		
+	return (0);
+}
