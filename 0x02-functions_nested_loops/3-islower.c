dbfda84c206b62fed22f0ce1df0e778fcaa11d4b
Marouane_aittamanaite <85191119+aittamanaite@users.noreply.github.com>
2023-06-22 00:56:40 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * int _islower - function
+ * int _islower - function prints
  * @c: varaible
  * Return: 1 success otherwise 0.
 */
 int _islower(int c)
 {
-	if (c >=97 && c<=122)
+	if (c >= 97 && c <= 122)
 		return (1);
 	return (0);
 }
