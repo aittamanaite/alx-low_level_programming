fb036c8ec4dcf651542e9e2fd04cd205b1cebf3d
Marouane_aittamanaite <85191119+aittamanaite@users.noreply.github.com>
2023-06-22 00:54:48 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,13 +6,6 @@
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-<<<<<<< HEAD
-<<<<<<< HEAD
 int _islower(int c);
-=======
-=======
-int _islower(int c);
->>>>>>> 69bcb709b6437d42e0dfd151d6cbd07a62baf86e
 
->>>>>>> 0967066a9a987ecc29adffeb150477fba8662aaa
 #endif
