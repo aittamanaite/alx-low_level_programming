7d47e2508660bd91b40c6f259ec7bce4aef95f46
Marouane_aittamanaite <85191119+aittamanaite@users.noreply.github.com>
2023-06-22 00:23:26 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,11 @@
-include"main.h"
+#include "main.h"
 /**
- *
- * print_alphabet_x10: prints 10 times the alphabits
- *
- *
- * */
+ * print_alphabet_x10 - Entry point
+ * Return: 0
+*/
 void print_alphabet_x10(void)
 {
-	int i;
-	int ch;
+	int i, ch;
 
 	for (i = 1; i <= 10; i++)
 	{
