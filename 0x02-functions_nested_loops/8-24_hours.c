38830421573a12953f2d729a8a90b7fa4b154313
aittamanaite <aittamanaite2001@gmail.com>
2023-06-21 18:54:12 -0700

    24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,22 +1,24 @@
 #include "main.h"
 /**
  *
- *
+ * jack_bauer - function
  *
  */
 void jack_bauer(void)
 {
   int hours,mins;
-  for(hours=0;hours<=24;hours++)
-  {
-      for( mins=0; mins<= 59;  mins++)
-      {
-        _putchar(hours/10 + 48);
-        _putchar(hours%10 +48);
-        _putchar(':');
-        _putchar(mins/10 + 48);
-        _putchar(mins%10 +48);
-        _putchar('\n');
-      }
-  }
+
+ 	 for(hours = 0; hours <= 24; hours++)
+	 {
+		 for(mins = 0; mins <= 59; mins++)
+		 {
+			 _putchar(hours / 10 + 48);
+                         _putchar(hours % 10 +48);
+                         _putchar(':');
+                         _putchar(mins / 10 + 48);
+                         _putchar(mins % 10 +48);
+                         _putchar('\n');
+		 }
+	 }
+        
 }
