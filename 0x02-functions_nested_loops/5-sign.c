b1eb0e9f5b64d061a930ad813c3b1d5397347c4c
Marouane_aittamanaite <85191119+aittamanaite@users.noreply.github.com>
2023-06-22 01:38:32 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,18 +5,20 @@
  *
  * @n: varaible
  * Return: 1 (success) 0 (fail).
- * */
+ */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar(43);
 		return (1);
-	}else if( n == 0)
+	}
+	else if( n == 0)
 	{
 		_putchar(48);
 		return (0);
-	}else
+	}
+	else
 	{
 		_putchar(45);
 		return (-1);
