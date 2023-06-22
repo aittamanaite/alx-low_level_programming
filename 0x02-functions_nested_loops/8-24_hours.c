85d1c4f285f50a3c3ca93f82e63276a926be45d8
Marouane_aittamanaite <85191119+aittamanaite@users.noreply.github.com>
2023-06-22 02:56:17 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,15 +8,15 @@ void jack_bauer(void)
 {
   int hours,mins;
 
- 	 for(hours = 0; hours <= 24; hours++)
+ 	 for(hours = 0; hours <= 23; hours++)
 	 {
 		 for(mins = 0; mins <= 59; mins++)
 		 {
-			 _putchar(hours / 10 + 48);
-                         _putchar(hours % 10 +48);
+			 _putchar((hours / 10) + 48);
+                         _putchar((hours % 10) +48);
                          _putchar(':');
-                         _putchar(mins / 10 + 48);
-                         _putchar(mins % 10 +48);
+                         _putchar((mins / 10) + 48);
+                         _putchar((mins % 10) +48);
                          _putchar('\n');
 		 }
 	 }
