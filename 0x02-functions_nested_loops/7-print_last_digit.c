a6230f34bb7f11fa6270b0f811ca4574f41c81bd
aittamanaite <aittamanaite2001@gmail.com>
2023-06-21 18:18:21 -0700

    print-last_digiy

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ *
+*/
+int print_last_digit(int n)
+{
+	int last_d;
+
+	if (n < 0)
+		last_d = (-1) * (n % 10) ;
+	else
+		last_d = n % 10;
+	_putchar(last_d + '0');
+	return (last_d);
+}
