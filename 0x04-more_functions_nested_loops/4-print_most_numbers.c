7d4e2f9cb1e4bb47e77603525da8b1ede9e11da1
Marouane_aittamanaite <85191119+aittamanaite@users.noreply.github.com>
2023-06-24 02:56:10 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,13 +7,13 @@
 */
 void print_most_numbers(void)
 {
-	int i;
+	int num;
 
-	for (i = 0; i <= 9; i++)
+	for (num = 0; num <= 9; num++)
 	{
-		if (i == 2 || i == 4)
+		if (num == 2 || num == 4)
 			continue;
-			_putchar(i + 48);
+			_putchar(num + 48);
 	}
 	_putchar('/n');
 }
