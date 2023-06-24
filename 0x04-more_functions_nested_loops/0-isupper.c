3b815c4de752f3c2be537882f81c04f771ef9479
Marouane_aittamanaite <85191119+aittamanaite@users.noreply.github.com>
2023-06-24 02:09:04 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _isdigit: fucntion return 1 or 0.
+ * _isupper: is a fucntion return 1 or 0.
  *
  * @c: variable
  *
  * Return: 1 success 0 (fail).
 */
-int _isdigit(int c)
+int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 		return (1);
