c16846f75e15ba371fff91458bd99bc22501da0d
aittamanaite <aittamanaite2001@gmail.com>
2023-06-23 18:47:21 -0700

    update

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_must_numbers - print all number but 2 and 4 not allowed
+ *
+ * Return: 1 success 0 (fail).
+*/
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4 )
+		{
+			continue;
+			_putchar(i + 48);
+		}
+	}
+	_putchar('/n');
+}
