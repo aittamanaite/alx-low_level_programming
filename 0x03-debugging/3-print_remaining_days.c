1f19ea7a3acdb438c492371cd37844006948a892
Marouane_aittamanaite <85191119+aittamanaite@users.noreply.github.com>
2023-06-24 01:22:03 +0100

    Update 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,6 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-<<<<<<< HEAD
 	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
 		if (month >= 2 && day >= 60)
@@ -34,7 +33,6 @@ void print_remaining_days(int month, int day, int year)
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
-=======
     if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
     {
 	    if (month >= 2 && day >= 60)
@@ -57,5 +55,4 @@ void print_remaining_days(int month, int day, int year)
 		    printf("Remaining days: %d\n", 365 - day);
 	    }
     }
->>>>>>> ba5b8707497587fb24e2b26fa8dc355324acdfe9
 }
