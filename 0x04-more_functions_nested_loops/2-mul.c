856d3f6bf564cf3ce8a366e87496380a474781f6
aittamanaite <aittamanaite2001@gmail.com>
2023-06-23 18:24:22 -0700

    update

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - is a fucntion return the mutiplication of two digits.
+ *
+ * @a: variable
+ *
+ * @b: varaible
+ *
+ * Return: 1 success 0 (fail).
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
