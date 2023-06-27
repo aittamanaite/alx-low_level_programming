233b38d12075c96ffd8cd0c98a00ae14d40db57f
aittamanaite <aittamanaite2001@gmail.com>
2023-06-26 18:26:36 -0700

    swap_int

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - check the code
+ *
+ * @a: varaible
+ * @b: varaible
+ * Return: Always 0.
+*/
+	void swap_int(int *a, int *b)
+	{
+		int tmp;
+
+		tmp = *a;
+		*a = *b;
+		*b = temp
+	}
