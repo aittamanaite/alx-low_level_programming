13b650487ee1e858d23a99b271bfbe2c12e11713
aittamanaite <aittamanaite2001@gmail.com>
2023-06-30 13:55:06 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,15 +12,15 @@
 */
 char *_strcat(char *dest, char *src)
 {
-	int c ,c2;
+	int c, c2;
 
-	c=0;
+	c = 0;
 	while (dest[c])
 		c++;
 
-	for (c2=0; c2<=src[c2]; c2++)
+	for (c2 = 0; c2 <= src[c2]; c2++)
 	{
-		dest[c++]=src[c2];
+		dest[c++] = src[c2];
 	}
 	return (dest);
 }
