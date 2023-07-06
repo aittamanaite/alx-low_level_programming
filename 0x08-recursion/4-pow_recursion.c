d6133adf1268ff061462bffc3cf0c34ed0061e95
Marouane_aittamanaite <85191119+aittamanaite@users.noreply.github.com>
2023-07-07 00:22:16 +0100

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,11 +12,10 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	else if (x == 1)
+	else if (y == 1)
 		return (x);
 	else if (y == 0)
 		return (1);
-	else
-		return (x * _pow_recusion(x, y - 1));
-
+	
+	return (x * _pow_recusion(x, y - 1));
 }
