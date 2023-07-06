a71b8a3c2c5693c512f30d092dad9ca42e4185b6
Marouane_aittamanaite <85191119+aittamanaite@users.noreply.github.com>
2023-07-06 22:44:41 +0100

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _puts_recursion - fucntion
+ * _print_rev_recursion - fucntion
  *
  * @s: parametre
  *
  * Return:0 (success).
  *
 */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s > '\0')
 	{
