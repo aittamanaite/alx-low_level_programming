a92a97afc647b28d791274dce9d519dbaa5db781
aittamanaite <aittamanaite2001@gmail.com>
2023-07-06 15:47:38 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * factorial - fucntion
+ *
+ * @n: parametre
+ *
+ * Return:0 (success).
+ *
+*/
+int factorial(int n)
+{
+	if ( n < 0 )
+		return -1;
+
+	else if (n == 0)
+		return 1;
+	else
+		return (n * factorial(n - 1));
+}
+
