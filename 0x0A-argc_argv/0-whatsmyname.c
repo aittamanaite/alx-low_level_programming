34f60b0b86ee6c313ccee698231c823eb6bc86e5
aittamanaite <aittamanaite2001@gmail.com>
2023-07-11 09:07:09 -0700

    whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,11 +6,9 @@
  * @argv : var
  * Return : 0
 */
-
-int main(int argc,char const *argv[])
+int main(int argc, char const *argv[])
 {
 	(void)argc;
-    	printf("%s\n",argv[0]);
-   	 return 0;
+	printf("%s\n", argv[0]);
+	return (0);
 }
-
