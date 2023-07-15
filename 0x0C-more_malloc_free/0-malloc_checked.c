4553edcf1a9777432909f1f801860f7bdfda5c33
Marouane_aittamanaite <85191119+aittamanaite@users.noreply.github.com>
2023-07-15 01:26:09 +0100

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * malloc_checked - allocates memory using malloc, exit(98) if it fails
+ * *malloc_checked - allocates memory using malloc, exit(98) if it fails
  * @b: size of the memory block to be allocated
  *
  * Return: pointer to the address of the memory block
