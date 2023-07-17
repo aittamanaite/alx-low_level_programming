e9a62f5a36dcb81d28ba023c1df6183069f17899
Marouane_aittamanaite <85191119+aittamanaite@users.noreply.github.com>
2023-07-17 21:15:38 +0100

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,8 +11,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
-		(*p).name=name;
-		(*p).age=age;
-		(*p).owner=owner;
+		(*d).name=name;
+		(*d).age=age;
+		(*d).owner=owner;
 	}
 }
