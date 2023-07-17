84f84729915315f0b2cd662308347fb01d86213f
Marouane_aittamanaite <85191119+aittamanaite@users.noreply.github.com>
2023-07-17 23:13:48 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 /**
-<<<<<<< HEAD
  * main -Entry point
-=======
  * main - it puts out output using put
  *
  * Description: a c programme
->>>>>>> 3da9adf23e8bba2774e54593ba975b9eadf8634f
  *
  * Return: Always 0
  *
