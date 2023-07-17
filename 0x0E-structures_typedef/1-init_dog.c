f5152388a6f960d992265bf2aa7356fb80b9d149
aittamanaite <aittamanaite2001@gmail.com>
2023-07-17 13:12:30 -0700

    init_dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+/**
+ * init_dog - function void
+ * @d: pointer
+ * @name: parameter
+ * @age:parameter
+ * @owner: parametere
+ * Return : 0(success)
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		(*p).name;
+		(*p).age;
+		(*p).owner;
+	}
+}
