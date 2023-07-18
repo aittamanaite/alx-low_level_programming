08a80bf7ecd4e16358c56c4e9ca74cba783b56ba
Marouane_aittamanaite <85191119+aittamanaite@users.noreply.github.com>
2023-07-18 14:22:11 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,14 @@
  *
  * Description:  just a dog structure lol!.
 */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
