fdf1b34750235f5f33fcd82a98b61001cdf04356
Marouane_aittamanaite <85191119+aittamanaite@users.noreply.github.com>
2023-07-22 01:31:03 +0100

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,14 +12,9 @@ int sum_them_all(const unsigned int n, ...)
 
 		if  (n == 0)
 			return (0);
-		else
-		{
 		va_start(args,  n);
 		for (int i = 0; i < n; i++)
-		{
-		sum += va_arg(args, int);
-		}
-		}
+			sum += va_arg(args, int);
 		va_end(args);
 		return (sum);
 }
