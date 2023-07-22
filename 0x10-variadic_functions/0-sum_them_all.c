d0e6f579dc71fcdd6f281b546b20a59638d1e890
aittamanaite <aittamanaite2001@gmail.com>
2023-07-21 17:07:59 -0700

    sum_all

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - function
+ * @n: var
+ * Return: int.
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	va_list args;
+
+	va_start(args,  n);
+
+	for (int i = 0; i  < n; i++)
+	{
+		if  (n == 0)
+			return (0);
+		sum += va_arg(args, int);
+	}
+	return (sum);
+}
